f2145846d16f003b624507234e8a3e0a4cb669d1
123qwe <1583678907@qq.com>
2025-02-08 09:13:17 +0800

    change

--- main.py
+++ main.py
@@ -1,27 +1,111 @@
 import torch
+import random
+import argparse
+import numpy as np
 import torchvision
+import datasets.transforms as T
+from datasets import build_dataset
 import torch.nn as nn
+from pathlib import Path
+import utils.misc as utils
+from models import build_model
+from typing import Optional, List
+from torch import Tensor
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
+
+    parser.add_argument('--dataset_file', default='coco')
+    parser.add_argument('--coco_path', type=str)
+
+    parser.add_argument('--seed', default=42, type=int)
+
 # device  select GPU or CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# 0：cls  1: detect    2: seg
+class1 = 0
 
 # dataset load
-
     # classify
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)))
-])
+def main(args):
 
-train_dataset = torchvision.datasets.CIFAR10(root='./CIFAR10',train=True,transform=transform,download=True)
-test_dataset = torchvision.datasets.CIFAR10(root='./CIFAR10',train=False,transform=transform,download=True)
+    seed = args.seed
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+
+    model, criterion, postprocessors = build_model(args)
+
+    model.to(device)
+    model_without_ddp = model
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('number of params:', n_parameters)
+
+    if class1 == 0:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)))
+        ])
+
+        train_dataset = torchvision.datasets.CIFAR10(root='./CIFAR10',train=True,transform=transform,download=True)
+        test_dataset = torchvision.datasets.CIFAR10(root='./CIFAR10',train=False,transform=transform,download=True)
+
+        train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    elif class1 == 1:
+        
+        # coco dataset
+        
+        dataset_train = build_dataset(image_set='train', args=args)
+        dataset_val = build_dataset(image_set='val', args=args)
+
+        # 1、batch_size
+        batch_size = True
+        if batch_size:
+            train_dataset = DataLoader(dataset_train, batch_size=2,
+                                        shuffle=True, collate_fn=utils.collate_fn)
+            test_dataset = DataLoader(dataset_val, batch_size=2,
+                                        shuffle=True, collate_fn=utils.collate_fn)
+
+        # 2、batch_sampler
+            # batch_sample   What?
+        else:
+            sampler_train = torch.utils.data.RandomSampler(dataset_train)
+            sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+
+            batch_sampler_train = torch.utils.data.BatchSampler(
+            sampler_train, args.batch_size, drop_last=True)
+
+        # collate_fn  What?
+            data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
+                                    collate_fn=utils.collate_fn, num_workers=args.num_workers)
+            data_loader_val = DataLoader(dataset_val, args.batch_size, sampler=sampler_val,
+                                    drop_last=False, collate_fn=utils.collate_fn, num_workers=args.num_workers)
+        
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
 
 # the length of dataset
@@ -97,3 +181,13 @@ for epoch in range(epochs):
     if epoch == 9:
         torch.save(model, f'model_{epoch}.pth')
         print('the model is saved!')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('DETR training and evaluation script', parents=[get_args_parser()])
+
+    args = parser.parse_args()
+
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
---
+++ utils/box_ops.py
@@ -0,0 +1,9 @@
+import torch
+from torchvision.ops.boxes import box_area
+
+
+def box_xyxy_to_cxcywh(x):
+    x0, y0, x1, y1 = x.unbind(-1)
+    b = [(x0 + x1) / 2, (y0 + y1) / 2,
+         (x1 - x0), (y1 - y0)]
+    return torch.stack(b, dim=-1)
